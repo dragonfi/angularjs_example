45bb3fe685f2a496565f8fd3aaac5875d3d00490
Bodor Dávid Gábor <david.gabor.bodor@gmail.com>
2015-06-11 14:15:49 +0200

    Add hohmannTransfer and parts required by it

--- js/dvCalc.js
+++ js/dvCalc.js
@@ -1,15 +1,63 @@
 (function(){
     angular.module('dvCalc', [])
-    .controller('HohmannCalc', HohmannCalc)
-    .directive('dvCalcHohmann', function(){
-        return {
-            restrict: "E",
-            templateUrl: "html/hohmannCalc.html",
-            controller: "HohmannCalc as ctrl",
+    .service('dvCalc', dvCalcService)
+
+    function dvCalcService(){
+        dvCalc = this;
+        var km = 1000;
+        var Mm = 1000*km;
+        this.km = km;
+        this.Mm = Mm;
+
+        this.newOrbitFromAltitude = function(body, pe_altitude, ap_altitude){
+            var pe = pe_altitude + body.radius;
+            var ap = ap_altitude + body.radius;
+            return new dvCalc.Orbit(body, pe, ap);
         };
-    });
+
+        this.Orbit = function(body, pe, ap){
+            this.body = body;
+            this.pe = pe;
+            this.ap = ap;
+            if (this.pe > this.ap){
+                var tmp = this.pe;
+                this.pe = this.ap;
+                this.ap = tmp;
+            };
+        };
+        this.Orbit.prototype.isSafeOrbit = function(){
+            var atmo_height = this.body.radius + this.body.atmospheric_height;
+            var safety_radius = atmo_height + 10*km;
+            return this.pe > safety_radius;
+        };
+        this.Orbit.prototype.speedAt = function(radius){
+            radius = (radius === 'pe') ? this.pe : radius;
+            radius = (radius === 'ap') ? this.ap : radius;
+            if (radius < this.pe || radius > this.ap){
+                throw {
+                    message: "Radius must be within range",
+                    orbit: this, radius: radius
+                };
+            };
+            var a2 = (this.ap + this.pe);
+            return Math.sqrt(this.body.gm * 2 * (1/radius - 1/a2));
+        };
+
+        this.hohmannTransfer = function(source, target){
+            if (target.pe < source.pe) {
+                var tmp = source;
+                source = target;
+                target = tmp;
+            };
+
+            var transfer = new dvCalc.Orbit(source.body, source.pe, target.ap);
+            var dv1 = Math.abs(transfer.speedAt('pe') - source.speedAt('pe'));
+            var dv2 = Math.abs(target.speedAt('ap') - transfer.speedAt('ap'));
+            return dv1 + dv2;
+        };
+    };
 
     HohmannCalc.$inject = ['$scope'];
     function HohmannCalc($scope){
-    }
+    };
 })();
--- js/dvCalc_test.js
+++ js/dvCalc_test.js
@@ -7,16 +7,77 @@ describe('dvCalc module', function() {
         $controller = _$controller_;
     }));
 
-    describe("HohmannCalc controller", function() {
-        var scope;
-        var ctrl;
+    describe("dvCalc service", function() {
+        var kerbin = {
+            name: "Kerbin",
+            radius: 600000,
+            atmospheric_height: 70000,
+            gm: 3.5316e12,
+        };
 
-        beforeEach(angular.mock.inject(function($rootScope){
-            scope = $rootScope.$new();
-            ctrl = $controller("HohmannCalc", {$scope: scope});
+        var dvCalc;
+        var km;
+        beforeEach(angular.mock.inject(function(_dvCalc_){
+            dvCalc = _dvCalc_;
+            km = dvCalc.km;
+            Mm = dvCalc.Mm;
         }));
 
-        it("should do something", function() {
+        describe("OrbitFromSurface constructor", function() {
+            it("should construct a new orbit given correct values", function(){
+                var o = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 100*km);
+                expect(o.isSafeOrbit()).toBe(true);
+                expect(o.pe).toBe(kerbin.radius + 100*km);
+                expect(o.ap).toBe(kerbin.radius + 100*km);
+            });
+            it("should swap pe and ap if pe is greater than ap", function(){
+                var o = dvCalc.newOrbitFromAltitude(kerbin, 200*km, 100*km);
+                expect(o.isSafeOrbit()).toBe(true);
+                expect(o.pe).toBe(kerbin.radius + 100*km);
+                expect(o.ap).toBe(kerbin.radius + 200*km);
+            });
+            it("should return the correct speed for circular", function(){
+                var o = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 100*km);
+                expect(o.speedAt('pe')).toBeCloseTo(2246.140, 3);
+                expect(o.speedAt('ap')).toBe(o.speedAt('pe'));
+            });
+            it("should return the correct speed for elliptical", function(){
+                var o = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 200*km);
+                expect(o.speedAt('pe')).toBeCloseTo(2319.803, 3);
+                expect(o.speedAt('ap')).toBeCloseTo(2029.828, 3);
+            });
+            it("should return the correct speed at Mun orbit", function(){
+                var o = new dvCalc.Orbit(kerbin, 12*Mm, 12*Mm);
+                expect(o.speedAt('pe')).toBeCloseTo(542.5, 1);
+                expect(o.speedAt('ap')).toBe(o.speedAt('pe'));
+            });
+        });
+
+        describe("hohmannTransfer", function() {
+            it("should calculate correct value and be reversible", function(){
+                var o1 = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 100*km);
+                var o2 = new dvCalc.Orbit(kerbin, 12*Mm, 12*Mm);
+                var o1_to_o2 = dvCalc.hohmannTransfer(o1, o2);
+                var o2_to_o1 = dvCalc.hohmannTransfer(o2, o1);
+                expect(o1_to_o2).toBeCloseTo(1203.975, 3);
+                expect(o1_to_o2).toBe(o2_to_o1);
+            });
+            it("should calculate correct value for elliptical", function(){
+                var o1 = dvCalc.newOrbitFromAltitude(kerbin, 200*km, 400*km);
+                var o2 = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 300*Mm);
+                var o1_to_o2 = dvCalc.hohmannTransfer(o1, o2);
+                var o2_to_o1 = dvCalc.hohmannTransfer(o2, o1);
+                expect(o1_to_o2).toBeCloseTo(802.93, 1);
+                expect(o1_to_o2).toBe(o2_to_o1);
+            });
+            it("should calculate correct value for elliptical 2", function(){
+                var o1 = dvCalc.newOrbitFromAltitude(kerbin, 100*km, 400*km);
+                var o2 = dvCalc.newOrbitFromAltitude(kerbin, 200*km, 300*Mm);
+                var o1_to_o2 = dvCalc.hohmannTransfer(o1, o2);
+                var o2_to_o1 = dvCalc.hohmannTransfer(o2, o1);
+                expect(o1_to_o2).toBeCloseTo(737.057, 3);
+                expect(o1_to_o2).toBe(o2_to_o1);
+            });
         });
     });
 });
